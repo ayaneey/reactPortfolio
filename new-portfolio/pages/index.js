6325f8a1b32f83e2d375c7d14a77a72b807440a5
Ayan Ahmed <ayanahmed@ayans-mbp.home>
2023-01-20 21:31:51 +0000

    setting up navbar

--- new-portfolio/pages/index.js
+++ new-portfolio/pages/index.js
@@ -1,5 +1,6 @@
 import { Main } from "next/document";
 import Head from "next/head";
+import { BsFillMoonStarsFill } from "react-icons/bs";
 
 function index() {
 	return (
@@ -10,8 +11,25 @@ function index() {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
-			<main>
-				<section className="bg-slate-400 h-screen"></section>
+			<main className="bg-white px-10">
+				<section className="min-h-screen">
+					<nav className="py-10 mb-12 flex justify-between">
+						<h1 className="text-xl">Ayan Ahmed</h1>
+						<ul className="flex items-center">
+							<li>
+								<BsFillMoonStarsFill className="cursor-pointer text-2xl" />
+							</li>
+							<li>
+								<a
+									className="bg-gradient-to-r from-gray-300 to-slate-400 text-white px-4 py-2 rounded-md ml-8"
+									href="#"
+								>
+									Resume
+								</a>
+							</li>
+						</ul>
+					</nav>
+				</section>
 			</main>
 		</div>
 	);
